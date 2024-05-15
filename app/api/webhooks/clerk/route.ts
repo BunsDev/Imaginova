1c52d6e31da426cc96aee20992404772c784cb6a
stugufaka <stugufaka@gmail.com>
2024-05-15 11:03:36 +0000

    update route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -58,32 +58,32 @@
 //   const eventType = evt.type;
 
 //   // CREATE
-// if (eventType === "user.created") {
-//   const { id, email_addresses, image_url, first_name, last_name, username } =
-//     evt.data;
+//   if (eventType === "user.created") {
+//     const { id, email_addresses, image_url, first_name, last_name, username } =
+//       evt.data;
 
-//   const user = {
-//     clerkId: id,
-//     email: email_addresses[0].email_address,
-//     username: username!,
-//     firstName: first_name,
-//     lastName: last_name,
-//     photo: image_url,
-//   };
+//     const user = {
+//       clerkId: id,
+//       email: email_addresses[0].email_address,
+//       username: username!,
+//       firstName: first_name,
+//       lastName: last_name,
+//       photo: image_url,
+//     };
 
-//   const newUser = await createUser(user);
+//     const newUser = await createUser(user);
 
-//   // Set public metadata
-//   if (newUser) {
-//     await clerkClient.users.updateUserMetadata(id, {
-//       publicMetadata: {
-//         userId: newUser._id,
-//       },
-//     });
+//     // Set public metadata
+//     if (newUser) {
+//       await clerkClient.users.updateUserMetadata(id, {
+//         publicMetadata: {
+//           userId: newUser._id,
+//         },
+//       });
+//     }
+
+//     return NextResponse.json({ message: "OK", user: newUser });
 //   }
-
-//   return NextResponse.json({ message: "OK", user: newUser });
-// }
 
 //   // UPDATE
 //   if (eventType === "user.updated") {
@@ -119,9 +119,6 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
-import { clerkClient } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -171,39 +168,10 @@ export async function POST(req: Request) {
 
   // Do something with the payload
   // For this guide, you simply log the payload to the console
-  const { id, ...attributes } = evt.data;
+  const { id } = evt.data;
   const eventType = evt.type;
-  console.log(evt.data);
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log("Webhook body:", body);
 
-  if (eventType === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } =
-      evt.data;
-
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    };
-
-    const newUser = await createUser(user);
-
-    // Set public metadata
-    if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser._id,
-        },
-      });
-    }
-
-    return new Response("", { status: 200 });
-  }
-
-  // console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  // console.log("Webhook body:", body);
-
-  // return new Response("", { status: 200 });
+  return new Response("", { status: 200 });
 }
