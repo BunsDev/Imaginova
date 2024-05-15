d3c13bafb9a69b115b3ab012b505e6b8481bef44
stugufaka <stugufaka@gmail.com>
2024-05-15 10:57:33 +0000

    update page.tsx, route.ts, Sidebar.tsx and user.actions.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -171,8 +171,9 @@ export async function POST(req: Request) {
 
   // Do something with the payload
   // For this guide, you simply log the payload to the console
-  const { id } = evt.data;
+  const { id, ...attributes } = evt.data;
   const eventType = evt.type;
+  console.log(evt.data);
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,72 +1,96 @@
-"use client"
+"use client";
 
-import { navLinks } from '@/constants'
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { Button } from '../ui/button'
+import { navLinks } from "@/constants";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
+import { createUser } from "@/lib/actions/user.actions";
 
 const Sidebar = () => {
   const pathname = usePathname();
-
+  const createUser_ = async () => {
+    const newUser = await createUser({
+      clerkId: "id",
+      email: "test@gmail.com",
+      username: "mendsalbert",
+      firstName: "fn",
+      lastName: "ln",
+      photo: "fdsf",
+    });
+  };
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
         <Link href="/" className="sidebar-logo">
-          <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
+          <Image
+            src="/assets/images/logo-text.svg"
+            alt="logo"
+            width={180}
+            height={28}
+          />
         </Link>
 
         <nav className="sidebar-nav">
           <SignedIn>
             <ul className="sidebar-nav_elements">
               {navLinks.slice(0, 6).map((link) => {
-                const isActive = link.route === pathname
+                const isActive = link.route === pathname;
 
                 return (
-                  <li key={link.route} className={`sidebar-nav_element group ${
-                    isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
-                  }`}>
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
                     <Link className="sidebar-link" href={link.route}>
-                      <Image 
+                      <Image
                         src={link.icon}
                         alt="logo"
                         width={24}
                         height={24}
-                        className={`${isActive && 'brightness-200'}`}
+                        className={`${isActive && "brightness-200"}`}
                       />
                       {link.label}
                     </Link>
                   </li>
-                )
+                );
               })}
-              </ul>
-
+            </ul>
 
             <ul className="sidebar-nav_elements">
               {navLinks.slice(6).map((link) => {
-                const isActive = link.route === pathname
+                const isActive = link.route === pathname;
 
                 return (
-                  <li key={link.route} className={`sidebar-nav_element group ${
-                    isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
-                  }`}>
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
                     <Link className="sidebar-link" href={link.route}>
-                      <Image 
+                      <Image
                         src={link.icon}
                         alt="logo"
                         width={24}
                         height={24}
-                        className={`${isActive && 'brightness-200'}`}
+                        className={`${isActive && "brightness-200"}`}
                       />
                       {link.label}
                     </Link>
                   </li>
-                )
+                );
               })}
 
               <li className="flex-center cursor-pointer gap-2 p-4">
-                <UserButton afterSignOutUrl='/' showName />
+                <UserButton afterSignOutUrl="/" showName />
               </li>
             </ul>
           </SignedIn>
@@ -77,9 +101,16 @@ const Sidebar = () => {
             </Button>
           </SignedOut>
         </nav>
+        <button
+          onClick={() => {
+            createUser_();
+          }}
+        >
+          Create user
+        </button>
       </div>
     </aside>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
