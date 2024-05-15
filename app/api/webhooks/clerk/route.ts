d04a7a713dcfded6c529c6c2b045e08883059832
stugufaka <stugufaka@gmail.com>
2024-05-15 20:09:13 +0000

    update route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -171,22 +171,22 @@ export async function POST(req: Request) {
   // Do something with the payload
   // For this guide, you simply log the payload to the console
   const { id } = evt.data;
-  if (evt.type === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } =
-      evt.data;
+  // if (evt.type === "user.created") {
+  //   const { id, email_addresses, image_url, first_name, last_name, username } =
+  //     evt.data;
 
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    };
+  //   const user = {
+  //     clerkId: id,
+  //     email: email_addresses[0].email_address,
+  //     username: username!,
+  //     firstName: first_name,
+  //     lastName: last_name,
+  //     photo: image_url,
+  //   };
 
-    const newUser = await createUser(user);
-    return NextResponse.json({ message: "OK", user: newUser });
-  }
+  //   const newUser = await createUser(user);
+  //   return NextResponse.json({ message: "OK", user: newUser });
+  // }
   const eventType = evt.type;
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log("Webhook body:", body);
