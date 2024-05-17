fd4e83e44640c8cfd9fb7036452f8c99fb908dbc
stugufaka <stugufaka@gmail.com>
2024-05-17 12:52:02 +0000

    update cn.ts

--- utils/cn.ts
+++ utils/cn.ts
@@ -1,6 +1,6 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-
+// function to merge classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
